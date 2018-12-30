456c6cdb77df77eeb50a2fcf3cf3787e984a3dad
Varlog <varlog.err@gmail.com>
2018-12-30 21:24:53 +0200

    add ubuntu 18.04 ansible installer

---
+++ ansible-install/ubuntu1804.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# can be used with:
+# * ubuntu bionic
+# * linux mint tara
+
+if which ansible >/dev/null; then
+    echo "Upgrading ansible"
+    apt-get --only-upgrade install -y ansible
+    exit 0
+fi
+
+source_list_path="/etc/apt/sources.list.d/ansible.list"
+
+echo "Installing software-properties-common and dirmngr"
+apt-get install -y software-properties-common dirmngr
+
+echo "Installing ansible"
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 6125E2A8C77F2818FB7BD15B93C4A3FD7BB9C367 \
+&& echo "deb http://ppa.launchpad.net/ansible/ansible/ubuntu bionic main" | tee "${source_list_path}" \
+&& echo "deb-src http://ppa.launchpad.net/ansible/ansible/ubuntu bionic main" | tee -a "${source_list_path}" \
+&& apt-get update \
+&& apt-get install -y ansible
